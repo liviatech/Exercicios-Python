a9f3d0939903981e5fa241ffe3c00669bcc06c9f
Livia Matias <150747124+liviatech@users.noreply.github.com>
2024-07-10 17:45:07 -0300

    conceitos básicos de python
    declaração e tipos de variáveis

---
+++ basico_python.py
@@ -0,0 +1,80 @@
+# '''1.FaçaumProgramaquepeçadoisnúmeros,realizeasprincipaisoperaçõessoma,subtração,multiplicação,divisão'''
+# numero1 = int( input('Digite seu numero 1: '))
+# numero2 =  int( input('Digite seu numero 2: '))
+
+# soma = numero1 + numero2
+# subtracao = numero1 - numero2
+# multiplicacao = numero1 * numero2
+# divisao = numero1 / numero2
+
+# print(f'O resultado da soma é {soma} O resultado da subtração é {subtracao} o resultado da multiplicação é {multiplicacao} e o resultado da divisão é {divisao}')
+
+
+
+
+# '''2.Peçaaousuárioparainformaroanodenascimento.Emseguida,calculeeimprimaaidadeatual.'''
+# numero1 = int( input('Digite o ano do seu nascimento : '))
+# ano = 2024 - numero1
+# print(f'O ano do seu nascimento é : {ano}')
+
+
+
+#3.FaçaumProgramaquepeçaaquantidadedequilômetros,transformeemmetros,centímetrosemilímetros.
+# numero1 = int( input('Quantos quilometros? '))
+# metros = numero1 * 1000
+# centimetros = numero1 * 100000
+# milimetros = numero1 * 1000000
+# print(f'O resultado em metros é:{metros} em centímetros é : {centimetros} em milimetros é : {milimetros}')
+
+
+
+#4.Recebadousuárioaquantidadedelitrosdecombustívelconsumidoseadistânciapercorrida.Calculeeimprimaoconsumomédioemkm/l
+
+# litros = float( input('Quantos litros de combustível foram gastos? '))
+# distancia = float( input('Qual a distância percorrida? '))
+# media =  distancia/litros
+# print(f'O consumo medio  é:{media} Km/l')
+
+
+
+'''5.Escrevaumprogramaquecalculeotempodeumaviagem.Façaumcomparativodomesmopercursodeavião,carroeônibus.Levandoemconsideração:●avião=600km/h●carro=100km/h●ônibus=80km/'''
+'''distancia = float( input('Qual a distância da viagem em Km? '))
+aviao = (distancia / 600 )
+carro = (distancia / 100)
+onibus= (distancia / 80)
+aviaomin = aviao * 60
+carromin = carro * 60
+onibusmin = onibus * 60
+print(f'O tempo da viagem de avião é : {aviaomin} minutos,  de carro é  {carromin}minutos,  de onibus é  {onibusmin} minutos.')'''
+
+# # 6.Soliciteaousuárioopesoemkgeaalturaemmetros.CalculeeimprimaoÍndicedeMassaCorporal(IMC)usandoafórmula:IMC=peso/(alturaxaltura)
+
+# peso = float( input('Informe o peso : '))
+# altura = float(input('Informe a altura : '))
+
+# imc = peso / (altura * altura)
+
+# print(f'O indice de massa corporal é {imc}')
+
+#7.FaçaumProgramaqueperguntequantovocêganhaporhoraeonúmerodehorastrabalhadasnomês.Calculeemostreototaldoseusalárionoreferidomês.
+
+# ganho_hora = float( input('Informe o valor por hora : '))
+# horas_trabalhadas = float( input('Informe a quantidade de horas trabalhadas : '))
+# salario = ganho_hora * horas_trabalhadas
+# print(f'O valor total do salário é {salario: .2f}')
+
+# # 8.Soliciteaousuárioonúmerodehorasdeexercíciofísicoporsemana.Calculeototaldecaloriasqueimadasemummês,considerandoumamédiade5caloriasporminutodeexercício.
+# hora_exercicios = float( input('Informe a quantidade de horas  de exercicios na semana : '))
+# minutos_semana = hora_exercicios *60
+# gasto_calorico = (minutos_semana * 5) * 4
+# print(f'A quantidade de queima calórca em 1 mês é : {gasto_calorico: .2f}')
+
+
+# 9.FaçaumProgramaqueutilize4variáveiscomopreferirenofinalprintumamensagemamigávelutilizandoasvariáveiscriadas.Exemplosdevariáveis:nome,idade,lugar,profissão....Exemploderetorno:OláMaria,prazerteconhecer.SoudeSãoPaulotambémeestoumigrandodeárea.Lembrandoqueparaoretornovamosusarprintcomasvariáveiscriadaseestetextoésomenteumexemplo,utilizemacriatividade.
+
+# nome = input('Digite seu nome:')
+# cor_cabelo  = input('Digite a cor do cabelo:')
+# estudo = input ('Digite o curso que está fazendo:')
+# ano_formatura = int(input('Digite o ano da sua formatura: '))
+# print(f'Olá {nome} seja muito bem vindo, é lindo seus cabelos {cor_cabelo} parabéns por estudar {estudo} você irá se formar em {ano_formatura}.')
+
